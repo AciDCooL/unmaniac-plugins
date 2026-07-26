1e0a155c884ae99e6c4658d9ced523d066d0610c
AciDCooL <acidcool@acidflix.eu>
2026-07-26 23:41:13 +0200

    Add CPU-patched detect_audio_language plugin

---
+++ detect_audio_language/init.d/install_deps.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Script is executed by the Unmanic container on startup to auto-install dependencies
+TARGET_DIR="/opt/venv"
+if [[ -f "$TARGET_DIR/pyvenv.cfg" && -f "$TARGET_DIR/bin/python3" ]]; then
+    # Venv case (Ubuntu 24 style or manual venv)
+    python_command="$TARGET_DIR/bin/python3"
+else
+    # System case (Ubuntu 22 style)
+    python_command="/usr/bin/python3"
+fi
+
+if ! command -v whisper &> /dev/null; then
+    echo "**** Installing whisper ****"
+    "$python_command" -m pip install -U openai-whisper
+else
+    echo "**** whisper already installed ****"
+fi
+
+tch=$("${python_command}" -m pip list | grep "^torch ")
+[[ ! $tch ]] && "$python_command" -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
